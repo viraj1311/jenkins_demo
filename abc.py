5525d3083842b62dd4f0b1c9a12a6a385614b08e
Viraj Patel <56950229+viraj1311@users.noreply.github.com>
2021-10-15 19:58:46 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,4 +1,6 @@
- n = int(input()) 
- print(n * 10) 
- except EOFError as e: 
- print(e)
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
